a48e55fa4b7fbe0df2c3613991dffb610b35dbed
oleg <o.monsvetov@gmail.com>
2018-05-21 17:55:18 +0300

    initial commit

---
+++ init.php
@@ -0,0 +1,15 @@
+<?php  
+//здесь будет описана логика 
+
+
+session_start();
+// соединимся с локолхостом под пользователем рут  без пароля к базе slabel 
+$conn = mysqli_connect('Localhost', 'root', '', 'slabel');
+
+// создадим переменную user  которая будет равна (если в сесии есть user ) непосредственно этому значению иначе NULL
+$user = isset($_SESSION['user']) ? $_SESSION['user'] : NULL;
+
+include 'tpl/base.html';
+
+?>
+
